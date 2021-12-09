1199e1e7a2ab7aa1cc31817dd942c8ab3cf29015
Beatriz Cruz <mllebia@live.com>
2021-12-09 09:31:13 -0300

    Django Girls project start

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\beatriz.cruz\Documents\CursoPython\DjangoGirls\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
